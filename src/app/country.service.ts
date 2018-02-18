2c15bae294e5f28da5c65bc828574037a389140b
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-02-18 17:23:46 -0600

    Changes in country service

--- src/app/country.service.ts
+++ src/app/country.service.ts
@@ -3,24 +3,28 @@ import {Observable} from 'rxjs/Observable';
 import {Country} from './country';
 import 'rxjs/add/operator/map';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {catchError} from 'rxjs/operators';
+import {catchError, tap} from 'rxjs/operators';
 import {of} from 'rxjs/observable/of';
+import 'rxjs/add/operator/catch';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 @Injectable()
 export class CountryService {
-  constructor(private http: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
   getCountries(): Observable<Country[]> {
-    return this.http.get<Country[]>('http://localhost:8000/api/countries').pipe(
-      // catchError(this.handleError('getHeroes', []))
-    );
+    return this.httpClient.get<Country[]>('http://localhost:8000/api/countries').pipe(
+      // tap( // Log the result or error
+      //   data => console.log(data),
+      //   error => console.error(error)
+      // ),
+      // catchError(this.handleError('getCountries', []))
+    ).catch( this.handleError('getCountries', []));
   }
-
   addCountry (country: Country): Observable<Country> {
-    return this.http.post<Country>('http://localhost:8000/api/countries', country, httpOptions).pipe();
+    return this.httpClient.post<Country>('http://localhost:8000/api/countries', country, httpOptions).pipe();
   }
 
   /**
@@ -33,10 +37,9 @@ export class CountryService {
     return (error: any): Observable<T> => {
 
       // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
+      // console.error(error); // log to console instead
       // TODO: better job of transforming error for user consumption
-      // this.log(`${operation} failed: ${error.message}`);
-
+      // this.log(`${operation} failed: ${error.message}`);;
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
