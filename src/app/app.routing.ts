71b588e3490db430cd9bd0b3be9b2d6f8b58999d
Brayan Arrieta Alfaro <brayan.arrieta.alfaro.96@gmail.com>
2018-02-18 14:30:52 -0600

    Create add country component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,12 @@
 import {Routes} from '@angular/router';
 import {CountriesComponent} from './countries/countries.component';
+import {CountryAddComponent} from './country-add/country-add.component';
 
 
 
 export const routes: Routes = [
   { path: 'countries', component: CountriesComponent },
-  /*Main route*/
+  { path: 'countries/create', component: CountryAddComponent },
+  // Main route
   { path: '', redirectTo: '/countries', pathMatch: 'full' }
 ];
