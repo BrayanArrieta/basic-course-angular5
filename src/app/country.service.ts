8681879274e7a841502c39a5aee8b55ebd48c746
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-02-18 20:28:04 -0600

    Fix catch error in server

--- src/app/country.service.ts
+++ src/app/country.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {Country} from './country';
 import 'rxjs/add/operator/map';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {catchError, tap} from 'rxjs/operators';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import {catchError, tap, retry} from 'rxjs/operators';
 import {of} from 'rxjs/observable/of';
 import 'rxjs/add/operator/catch';
+import {ErrorObservable} from 'rxjs/observable/ErrorObservable';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -16,12 +17,14 @@ export class CountryService {
 
   getCountries(): Observable<Country[]> {
     return this.httpClient.get<Country[]>('http://localhost:8000/api/countries').pipe(
-      // tap( // Log the result or error
-      //   data => console.log(data),
-      //   error => console.error(error)
-      // ),
-      // catchError(this.handleError('getCountries', []))
-    ).catch( this.handleError('getCountries', []));
+      retry(1), // retry a failed request up to 3 times
+      catchError(this.handleError) // then handle the error
+    );
+    // .catch( this.handleError('getCountries', []));
+    // tap( // Log the result or error
+    //   data => console.log(data),
+    //   error => console.error(error)
+    // ),
   }
   addCountry (country: Country): Observable<Country> {
     return this.httpClient.post<Country>('http://localhost:8000/api/countries', country, httpOptions).pipe();
@@ -33,15 +36,34 @@ export class CountryService {
    * @param operation - name of the operation that failed
    * @param result - optional value to return as the observable result
    */
-  private handleError<T> (operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
+  // private handleError<T> (operation = 'operation', result?: T) {
+  //   return (error: any): Observable<T> => {
+  //
+  //     // TODO: send the error to remote logging infrastructure
+  //     // console.error(error); // log to console instead
+  //     // TODO: better job of transforming error for user consumption
+  //     // this.log(`${operation} failed: ${error.message}`);;
+  //     // Let the app keep running by returning an empty result.
+  //     return of(result as T);
+  //   };
+  // }
 
-      // TODO: send the error to remote logging infrastructure
-      // console.error(error); // log to console instead
-      // TODO: better job of transforming error for user consumption
-      // this.log(`${operation} failed: ${error.message}`);;
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
+  /**
+   * Handle for HttpClient
+   */
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an ErrorObservable with a user-facing error message
+    return new ErrorObservable(
+      'Something bad happened; please try again later.');
   }
 }
